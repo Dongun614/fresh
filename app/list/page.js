93f1b2cdd714828f68e0c5df64ee2671aace6154
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-03 14:55:23 +0900

    useState 배열로 관리하기

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 
 export default function List(){
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-  let [수량, 수량변경] = useState(0)
+  let [수량, 수량변경] = useState([0, 0, 0])
   // let b = 어레이.map(function(a, i){
   //   return 10
   // })
@@ -12,18 +12,23 @@ export default function List(){
   return (
     <div>
        <h4 className="title">상품목록</h4>
+
        {
         상품.map((a, i)=>{
           return(
             <div className="food" key={i}>
               <img src={`/food${i}.png`} alt={ a } className="food-img"></img>
               <h4>{ 상품[i] } $40</h4>
-              <span> { 수량 } </span>
+              <span> { 수량[i] } </span>
               <button onClick={()=>{
-                { 수량변경( 수량+1 ) }
+                let copy = [...수량]
+                copy[i]++
+                수량변경(copy)
               }}> + </button>
               <button onClick={()=>{
-                { 수량변경(수량-1) }
+                let copy = [...수량]
+                copy[i]--
+                수량변경(copy)
               }}> - </button>
             </div>
           )
