8fa47f1452d3a97d46d9e57cf2412c4334a5c70d
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-03 10:55:35 +0900

    Map 배우기

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,22 @@
 export default function List(){
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let 어레이 = [2,3,4]
+  // let b = 어레이.map(function(a, i){
+  //   return 10
+  // })
+
   return (
     <div>
        <h4 className="title">상품목록</h4>
-       <div className="food">
-        <h4>상품 1 $40</h4>
-       </div>
-       <div className="food">
-        <h4>상품2 $40</h4>
-       </div>
+       {
+        상품.map((a, i)=>{
+          return(
+            <div className="food">
+              <h4>{ 상품[i] } $40</h4>
+            </div>
+          )
+        })
+       }
     </div>
   )
 }
