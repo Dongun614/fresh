f2d418751a576b3cb3dfd877419c29f6766901cd
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-03 11:51:38 +0900

    컴포넌트 문법 배우기

---
+++ app/cart/data.js
@@ -0,0 +1,3 @@
+let age = 20;
+let name = "park"
+export {age, name};
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,24 @@
+import {age, name} from './data.js'
+import Hello from './hello.js'
+
 export default function Cart() {
   return (
     <div>
-      <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+        <Hello/>
+        <h4 className="title">Cart</h4>
+        <CartItem/>
+        <CartItem/>
+        <CartItem/>
     </div>
   )
 } 
+
+function CartItem(){
+    return(
+        <div className="cart-item">
+            <p>상품명 { age }</p>
+            <p>$40</p>
+            <p>1개</p>
+      </div>
+    )
+}
